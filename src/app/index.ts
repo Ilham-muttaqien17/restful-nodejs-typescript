95ade68acd8b7b999661263ed41d0ee58694d5ff
Ilham Muttaqien <imuttaqien17@gmail.com>
2023-12-27 11:43:41 +0700

    chore: add helmet to enhance the app security
    Signed-off-by: Ilham Muttaqien <imuttaqien17@gmail.com>

--- src/app/index.ts
+++ src/app/index.ts
@@ -3,16 +3,28 @@ import express from 'express';
 import publicRoutes from '@src/routes/public';
 import privateRoutes from '@src/routes/private';
 import limiter from '@src/middlewares/rate-limiter';
+import helmet from 'helmet';
 
 const app = express();
 
+// Parse request body to json
 app.use(express.json());
+// Rate limiter for each request based on ip address
 app.use(limiter);
+// Secure Express by set HTTP response headers
+app.use(helmet());
 
 // Public route
 app.use('/api', publicRoutes);
 
 // Need auth
 app.use('/api', privateRoutes);
+
+app.use('*', (req, res) => {
+  res.status(404).send({
+    status: 404,
+    message: 'Resouce not found'
+  });
+});
 
 export default app;
